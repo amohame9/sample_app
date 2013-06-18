b99c50a9c869ef2a1fbf1cdb13dac4776e8361d5
Abdi Shakur <abdishakur@outlook.com>
2013-06-18 16:29:53 +0100

    Finnish Sign up form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,16 +11,56 @@ describe "User Pages" do
   	it { should have_selector('title', text: full_title('Sign Up'))}
   end
 
-  	it "should have the right links on the layout" do
-  		visit root_path
-  		click_link  "About"
-  		page.should have_selector 'title', text: full_title('About Us')
-  		click_link  "Help"
-  		page.should have_selector 'title', text: full_title('Help')
-  		click_link  "Contact"
-  		page.should have_selector 'title', text: full_title('Contact')
-  		click_link  "Home"
-  		click_link  "Sign up Now!"
-  		page.should have_selector 'title', text: full_title('Sign Up')
-  	end
+  describe "Profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+
+  	before { visit user_path(user) }
+
+  	  	it { should have_selector('h1', text: user.name) }
+  		it { should have_selector('title', text: user.name)}
+
+  end
+
+  describe "signup" do
+
+    before { visit signup_path}
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count) 
+      end 
+
+      describe "after submission" do  
+        before { click_button submit}
+
+        it { should have_selector('title', text: 'Sign Up') }
+        it { should have_content('error') }
+        it { should_not have_content('password digest')}
+      end    
+    end
+    
+    describe "with valid information" do
+
+      before do
+        fill_in "Name",          with: "Example User"
+        fill_in "Email",         with: "user@example.com"
+        fill_in "Password",      with: "foobar"
+        fill_in "Confirmation",  with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "After saving a user" do
+        before { click_button submit}
+
+        let(:user) { User.find_by_email("user@example.com")  }
+
+        it { should have_selector('title', text: user.name) }        
+      end
+    end
+
+  end  	
 end
