0ba1258e2e0096766c2ab856571d131fed85be9d
Abdi Shakur <abdishakur@outlook.com>
2013-06-25 21:09:41 +0100

    finish updating users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,47 @@ describe "User Pages" do
   
   subject { page }
 
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user)  }
+
+    before do
+      sign_in user
+      30.times { FactoryGirl.create(:user) }
+      visit users_path
+    end
+    
+    it { should have_selector('title', text: 'All users')}
+    it { should have_selector('title', text: 'All users')}
+
+    it "should list each user" do
+      User.paginate(page:1).each do |user|
+        page.should have_selector('li', text: user.name)  
+      end  
+    end
+
+    it { should have_selector('div.pagination') }
+
+  end
+
+  describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+      end
+    end
+
   describe	"sign up page" do
   	before { visit signup_path }
 
@@ -16,7 +57,7 @@ describe "User Pages" do
 
   	before { visit user_path(user) }
 
-  	  	it { should have_selector('h1', text: user.name) }
+  	  it { should have_selector('h1', text: user.name) }
   		it { should have_selector('title', text: user.name)}
 
   end
@@ -65,4 +106,46 @@ describe "User Pages" do
     end
 
   end  	
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create (:user)  }
+
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      
+
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails')}
+      
+    end
+    
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "new name" }
+      let(:new_email) { "new@example.com"  }
+      before do
+        fill_in  "Name", with: new_name
+        fill_in  "Email", with: new_email
+        fill_in  "Password", with: user.password
+        fill_in  "Confirm Password",  with: user.password
+        click_button "Save changes"
+      end
+      
+      it { should have_selector('title', text: new_name) }
+      it { should have_link('Sign out', href: signout_path)}
+      it { should have_selector('div.alert.alert-success') } 
+      specify {user.reload.name.should == new_name}
+      specify {user.reload.email.should == new_email}
+    end
+  end
 end
